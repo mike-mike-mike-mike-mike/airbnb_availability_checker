bbbdd3337a400d15f9ba2c01a795ee49040dc6ee
Michael Lambert <michael.lambert@appfolio.com>
2023-04-12 13:53:32 -0600

    Create static program to test
    Put the pieces together to test on a room of interest

---
+++ src/__main__.py
@@ -0,0 +1,12 @@
+from url_builder import UrlBuilder
+from availability_checker import AvailablityChecker
+
+def main():
+    # https://www.airbnb.com/rooms/50617365?check_in=2023-07-02&check_out=2023-07-06&adults=2
+    room_url = UrlBuilder('50617365', '2023-07-02', '2023-07-06').build()
+    is_available = AvailablityChecker(room_url).check_availability()
+
+    print(f"The room is {'not ' if not is_available else ''}available for those dates")
+
+if __name__ == '__main__':
+    main()
