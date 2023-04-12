5b34631d3c041a221ad74a281fa6115cca12a22d
Michael Lambert <michael.lambert@appfolio.com>
2023-04-12 14:30:04 -0600

    Move modules to subfolders

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,8 @@
-from url_builder import UrlBuilder
-from availability_checker import AvailablityChecker
+from util.url_builder import UrlBuilder
+from web.availability_checker import AvailablityChecker
 
 def main():
-    # https://www.airbnb.com/rooms/50617365?check_in=2023-07-02&check_out=2023-07-06&adults=2
+    # i.e. https://www.airbnb.com/rooms/50617365?check_in=2023-07-02&check_out=2023-07-06&adults=2
     room_url = UrlBuilder('50617365', '2023-07-02', '2023-07-06').build()
     is_available = AvailablityChecker(room_url).check_availability()
 
