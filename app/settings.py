b45f6606f5136a09ffddfdb0db828edbe298a699
Michael Lambert <michael.lambert@appfolio.com>
2025-01-21 21:57:37 -0800

    Enable actual sending of pw reset email

--- app/settings.py
+++ app/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,4 +142,11 @@ LOGIN_REDIRECT_URL = "trips:trip_list"
 LOGOUT_REDIRECT_URL = "users:login"
 
 # Email settings
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = os.getenv(
+    "EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend"
+)
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "default")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "default")
