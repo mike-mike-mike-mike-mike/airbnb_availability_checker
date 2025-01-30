20ab8ab8cb5aaa571e79091392f03aae0f7fcb93
Michael Lambert <michael.lambert@appfolio.com>
2025-01-29 20:07:27 -0800

    Replace sqlite with postgres

--- app/settings.py
+++ app/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "availability_checker",
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
